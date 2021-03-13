270ca06daeeb0cc204497dd8653b6e7c5ef7123f
Alex Berg <abermek@gmail.com>
2021-03-13 07:24:28 +0200

    Missing Files

---
+++ src/Form/DataTransformer/ProductSlugTransformer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form\DataTransformer;
+
+use App\Entity\Product;
+use App\Service\Repository\Doctrine\ProductRepository;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class ProductSlugTransformer implements DataTransformerInterface
+{
+    public function __construct(private ProductRepository $repository)
+    {
+    }
+
+    public function transform($value): string
+    {
+        /** @var Product $value */
+        if ($value === null || $value->getSlug() === null) {
+            return '';
+        }
+
+        return $value->getSlug();
+    }
+
+    public function reverseTransform($value): ?Product
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        $product = $this->repository->findOneBySlug($value);
+
+        if (!$product) {
+            throw new TransformationFailedException(
+                sprintf('A Product with slug "%s" does not exist!', $value)
+            );
+        }
+
+        return $product;
+    }
+}
---
+++ src/Form/Type/Product/SlugType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form\Type\Product;
+
+use App\Form\DataTransformer\ProductSlugTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SlugType extends AbstractType
+{
+    public function __construct(private ProductSlugTransformer $transformer)
+    {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->addModelTransformer($this->transformer);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['invalid_message' => 'The selected Product does not exist']);
+    }
+
+    public function getParent(): string
+    {
+        return TextType::class;
+    }
+}
